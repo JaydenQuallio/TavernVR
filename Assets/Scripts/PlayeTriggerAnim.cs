1848197d96082f3f37d1797c356f14693a7aa23d
Jayden Quallio <52089788+JaydenQuallio@users.noreply.github.com>
2024-04-10 02:19:45 -0600

    Frog Changes
    Added new idle anim
    Added new mat for boxers

--- Assets/Scripts/PlayeTriggerAnim.cs
+++ Assets/Scripts/PlayeTriggerAnim.cs
@@ -7,18 +7,24 @@ public class PlayeTriggerAnim : MonoBehaviour
 	[SerializeField] Animation animations;
 	[SerializeField] AnimationClip clip1, clip2;
 
+	bool isIn;
+
 	private void OnTriggerEnter(Collider coll)
 	{
-
+		if(isIn)
+		return;
 		animations.clip = null;
 		animations.clip = clip1;
 		animations.Play();
+		isIn = true;
 	}
 
 	private void OnTriggerExit(Collider coll)
 	{
+
 		animations.clip = null;
 		animations.clip = clip2;
 		animations.Play();
+		isIn = false;
 	}
 }
